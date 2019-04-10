82c19bc84049a510857ca6f489b989c105210581
blue-star-007 <akiraaiso@outlook.com>
2019-04-10 14:25:06 -0400

    Clean up test case

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,23 +2,25 @@ describe("index.js", () => {
   it("has a functional expression called divide", () => {
     expect(divide).to.exist
   })
+
   it("divide divides 2000 by 100", () => {
     let x = 2
     expect(divide).to.equal(20)
   })
-  
+
   it("has an arrow function called square", () => {
     expect(square).to.exist
   })
+
   it("square arrow function multiplies a number times itself", () => {
     let x = 2
     expect(square(x)).to.equal(4)
   })
-  // square multiplies x by itself
 
   it("has an arrow function called add", () => {
     expect(add).to.exist
   })
+
   it("add arrow function takes two parameters and adds them together", () => {
     let a = 3
     let b = 4
